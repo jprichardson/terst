fe0be3558f0a5b5ecd5ef5676569c1db16c81617
JP Richardson <jprichardson@gmail.com>
2013-10-22 18:36:28 -0500

    added tests

--- test/node.test.js
+++ test/node.test.js
@@ -1,2 +1,51 @@
-var terst = require('terst')
+var terst = require('../lib/terst')
 
+describe('terst', function() {
+  describe('+ T', function() {
+    describe('> when truthy', function() {
+      it('should not have any errors', function() {
+        var err = 0
+        try { T (true) } catch(e) { console.log(e); err += 1}
+        try { T ({}) } catch(e) { console.log(e); err += 1}
+        try { T (1) } catch(e) { console.log(e); err += 1}
+
+        if (err !== 0) throw new Error(err + ' should be 0') 
+      })
+    })
+
+    describe('> when falsey', function() {
+      it('should have any errors', function() {
+        var err = 0
+        try { T (false) } catch(e) { err += 1}
+        try { T (0) } catch(e) { err += 1}
+        try { T ('') } catch(e) { err += 1}
+
+        if (err !== 3) throw new Error(err + ' should not be 0') 
+      })
+    })
+  })
+
+  describe('+ F', function() {
+    describe('> when truthy', function() {
+      it('should not have any errors', function() {
+        var err = 0
+        try { F (true) } catch(e) { err += 1}
+        try { F ({}) } catch(e) {  err += 1}
+        try { F (1) } catch(e) {  err += 1}
+
+        if (err !== 3) throw new Error(err + ' should not be 0') 
+      })
+    })
+
+    describe('> when falsey', function() {
+      it('should have any errors', function() {
+        var err = 0
+        try { F (false) } catch(e) { err += 1}
+        try { F (0) } catch(e) { err += 1}
+        try { F ('') } catch(e) { err += 1}
+
+        if (err > 0) throw new Error(err + ' should be 0') 
+      })
+    })
+  })
+})
